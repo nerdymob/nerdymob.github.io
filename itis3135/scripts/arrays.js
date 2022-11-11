158e278333b599acdccc2e50cfa3510dcc24111d
nerdymob <103454704+nerdymob@users.noreply.github.com>
2022-11-11 09:36:27 -0500

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -6,13 +6,13 @@ document.add-salaries.focus();
 function addSalary() {
     let dropdownMenu = document.getElementById("employee_name");
     let name = dropdownMenu.options[dropdownMenu.selectedIndex].innerHTML;
-    let sal = parseFloat(prompt("Please enter their salary."));
+    let salary = parseFloat(prompt("Please enter their salary."));
 
-    if (isNaN(sal)) {
+    if (isNaN(salary)) {
         alert("Please enter a valid number without a $");
     } else {
        person.push(name);
-       salaries.push(sal); 
+       salaries.push(salary); 
     }
 }
 
@@ -37,14 +37,14 @@ function displayResults() {
 }
 
 function displaySalary() {
-    let salariesTable = document.getElementById('data');
+    let table = document.getElementById('data');
 
-    while (salariesTable.rows.length > 1){
-       salariesTable.deleteRow(1);
+    while (table.rows.length > 1){
+       table.deleteRow(1);
     }
 
     for (let i = 0; i < person.length; i++) {
-        let row = salariesTable.insertRow(salariesTable.rows.length);
+        let row = table.insertRow(table.rows.length);
         let personCell = row.insertCell(0);
         let salariesCell = row.insertCell(1);
         personCell.innerHTML = person[i];
